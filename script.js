77c0a73a108dfcbc28faae68e74eb672f8673a4e
carmanoli <57845866+carmanoli@users.noreply.github.com>
2022-08-07 15:22:34 +0100

    Added marquee

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+let quoteData;
 
-/*
 $(document).ready(() => {
-  alert("click");
-  $('#memoria').click(function(event){
-    loadPage(event.target.id);
-  });
-})
-*/
+  getQuoteData();
 
-let url = location.href;
+
+
+  
+})
 
 function loadPage(menuId) {
   $('#content').load(`${menuId}\\${menuId}.html`);
 }
+
+// =====================================================================
+// Quotes
+const quoteSettings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://type.fit/api/quotes",
+  "method": "GET"
+}
+
+function getQuoteData(){
+  $.ajax(quoteSettings)
+  .done(function (response) {
+    quoteData = JSON.parse(response);
+    console.log(quoteData);
+  })
+  .done(function () {
+    let quote = JSON.stringify(quoteData[Math.floor(Math.random()*quoteData.length)].text);
+    let author = JSON.stringify(quoteData[Math.floor(Math.random()*quoteData.length)].author);
+    // alert (quote + " " + author);
+    $('#scroll-text').text(quote + " " + author.replace(/['"]+/g, ''));
+
+    // we have to reset marquee animation
+    //$('#scroll-text').stop(true, true);
+
+  })
+  
+  ;
+}
+
